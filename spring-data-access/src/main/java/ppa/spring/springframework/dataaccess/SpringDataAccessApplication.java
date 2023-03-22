633f4900bd7153ec21b11842c782e20a35016ac7
ppavia <ppavia@protonmail.com>
2023-03-22 10:46:15 +0100

    spring data

---
+++ spring-data-access/src/main/java/ppa/spring/springframework/dataaccess/SpringDataAccessApplication.java
@@ -0,0 +1,12 @@
+package ppa.spring.springframework.dataaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataAccessApplication {
+
+    public static void main (String... args) {
+        SpringApplication.run(SpringDataAccessApplication.class, args);
+    }
+}
