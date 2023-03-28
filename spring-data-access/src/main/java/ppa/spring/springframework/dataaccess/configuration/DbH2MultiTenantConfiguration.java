413f8e9ea930f8917337dd37b768bb4293634db4
pierre pavia <ppavia@protonmail.com>
2023-03-28 19:10:53 +0200

    add multitenancy manager

--- spring-data-access/src/main/java/ppa/spring/springframework/dataaccess/configuration/DbH2MultiTenantConfiguration.java
+++ spring-data-access/src/main/java/ppa/spring/springframework/dataaccess/configuration/DbH2MultiTenantConfiguration.java
@@ -1,7 +1,6 @@
 package ppa.spring.springframework.dataaccess.configuration;
 
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -10,7 +9,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.UserCredentialsDataSourceAdapter;
