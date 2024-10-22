06a549ee68f9594cbfaf23619d9bf47e793838c2
ppavia <ppavia@protonmail.com>
2024-10-22 12:40:50 +0200

    daily commit

---
+++ ia/src/main/java/ppa/spring/springframework/ia/IaApplication.java
@@ -0,0 +1,13 @@
+package ppa.spring.springframework.ia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IaApplication.class, args);
+	}
+
+}
